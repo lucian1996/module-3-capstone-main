87ce4a571c659c8a7957eec9148a73e3d91d5ac2
Elijah Grantham <elijahrgrantham@gmail.com>
2022-12-06 11:19:08 -0500

    jdbc progress

--- capstone/java/src/main/java/com/techelevator/dao/ItemDao.java
+++ capstone/java/src/main/java/com/techelevator/dao/ItemDao.java
@@ -2,13 +2,17 @@ package com.techelevator.dao;
 
 import com.techelevator.model.Item;
 
+import java.util.List;
+
 public interface ItemDao {
 
-    Item getItem(int listId, int itemId);
+    Item getItemById(int listId, int itemId);
 
     boolean createItem(int listId);
 
     boolean deleteItem(int itemId);
 
     boolean updateItem(Item item);
+
+    List<Item> listItems(int listId);
 }
--- capstone/java/src/main/java/com/techelevator/dao/jdbc/JdbcItemDao.java
+++ capstone/java/src/main/java/com/techelevator/dao/jdbc/JdbcItemDao.java
@@ -2,10 +2,13 @@ package com.techelevator.dao.jdbc;
 
 import com.techelevator.dao.ItemDao;
 import com.techelevator.model.Item;
+import com.techelevator.model.User;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Component
@@ -13,24 +16,44 @@ public class JdbcItemDao implements ItemDao {
     private JdbcTemplate jdbcTemplate;
 
     @Override
-    public Item getItem(int listId, int itemId) {
-        return null;
+    public Item getItemById(int listId, int itemId) {
+        String sql = "SELECT item FROM list WHERE list_id = ? AND item_id = ?";
+        Item item;
+        item = jdbcTemplate.queryForObject(sql, Item.class, listId, itemId);
+        return item;
     }
 
+    //TODO
     @Override
     public boolean createItem(int listId) {
-        return false;
+        String sql = "INSERT INTO items (item_id, list_id, claimed_id) VALUES" +
+                " (DEFAULT, ?, ?) RETURNING user_id";
+        return true;
     }
 
+    //TODO
     @Override
     public boolean deleteItem(int itemId) {
         return false;
     }
 
+    //TODO
     @Override
     public boolean updateItem(Item item) {
         return false;
     }
+
+    @Override
+    public List<Item> listItems(int listId) {
+    List<Item> items = new ArrayList<>();
+    String sql = "SELECT * FROM items WHERE list_id = ?";
+    SqlRowSet results = jdbcTemplate.queryForRowSet(sql, listId);
+        while (results.next()) {
+        Item item = mapRowToItem(results);
+        items.add(item);
+    }
+        return items;
+}
 
     private Item mapRowToItem(SqlRowSet rs) {
         Item item = new Item();
