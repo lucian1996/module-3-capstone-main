2f17c439ceef90e3a1551b65235e2f1bce288278
Daniel Owens <dowens2889@gmail.com>
2022-12-06 11:15:00 -0500

    daniel working database

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -13,25 +13,25 @@ CREATE TABLE users (
 
 DROP SEQUENCE IF EXISTS seq_group_id, seq_list_id, seq_list_item_id;
 
-CREATE SEQUENCE seq_group_id,
+CREATE SEQUENCE seq_group_id
 	INCREMENT BY 1
 	START WITH 1001
 	NO MAXVALUE;
 CREATE TABLE  groups (
-	group_id int NOT NULL DEFUALT nextval('seq_group_id'),
+	group_id int NOT NULL DEFAuLT nextval('seq_group_id'),
 	group_name varchar(50) NOT NULL,
 	group_owner int NOT NULL,
 	CONSTRAINT PK_group PRIMARY KEY (group_id),
-	CONSTRAINT FK_group_owner FOREIGN KEY (group_onwer) REFERENCES users (user_id);
+	CONSTRAINT FK_group_owner FOREIGN KEY (group_onwer) REFERENCES users (user_id)
 );
 
 CREATE TABLE group_member (
     user_id int NOT NULL,
     group_id int NOT NULL,
     date_joined varcar(50) NOT NULL,
-    CONSTRAINT PK group_member PRIMARY KEY (user_id, group_id),
-    CONSTRAINT FK_group_member_user FOREIGN KEY user_id REFERENCES users (user_id),
-    CONSTRAINT FK_group_member_group FOREIGN KEY group_id REFERENCES groups (group_id)
+    CONSTRAINT PK_group_member PRIMARY KEY (user_id, group_id),
+    CONSTRAINT FK_group_member_user FOREIGN KEY (user_id) REFERENCES users (user_id),
+    CONSTRAINT FK_group_member_group FOREIGN KEY (group_id) REFERENCES groups (group_id)
 );
 
 CREATE SEQUENCE seq_list_id
@@ -39,12 +39,12 @@ CREATE SEQUENCE seq_list_id
 	START WITH 2001
 	NO MAXVALUE;
 CREATE TABLE list (
-	list_id int NOT NULL DEFAULT nextval('seq_list_id')
+	list_id int NOT NULL DEFAULT nextval('seq_list_id'),
 	group_id int NOT NULL,
 	list_title varchar(50) NOT NULL,
 	description varchar(500),
 	CONSTRAINT PK_list PRIMARY KEY (list_id),
-	CONSTRAINT FK_list_group FOREIGN KEY group_id REFERENCES groups (group_id)
+	CONSTRAINT FK_list_group FOREIGN KEY (group_id) REFERENCES groups (group_id)
 );
 
 CREATE SEQUENCE seq_list_item_id
@@ -55,14 +55,14 @@ CREATE TABLE list_item (
     list_item_id int NOT NULL DEFAULT nextval('seq_list_item_id'),
     list_id int NOT NULL,
     claimed int,
-    date_modified NOT NULL,
+    date_modified varchar(50) NULL,
     quantity int NOT NULL,
     last_modifier int NOT NULL,
     description varchar(500),
     CONSTRAINT PK_list_item PRIMARY KEY (list_item_id),
-    CONSTRAINT FK_list_item_list FOREIGN KEY list_id REFERENCES list (list_id),
-    CONSTRAINT quantity chk_quantity CHECK (quantity > 0)
+    CONSTRAINT FK_list_item_list FOREIGN KEY (list_id) REFERENCES list (list_id),
+    CONSTRAINT chk_quantity CHECK (quantity > 0)
     );
-)
+
 
 COMMIT TRANSACTION;
