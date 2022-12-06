f8ce5e34bf95d93fa6711a0b867cd3451cf3ace1
Daniel Owens <dowens2889@gmail.com>
2022-12-06 13:18:51 -0500

    added custom exceptions

---
+++ capstone/java/src/main/java/com/techelevator/dao/exceptions/CreateException.java
@@ -0,0 +1,7 @@
+package com.techelevator.dao.exceptions;
+
+public class CreateException extends RuntimeException{
+    public CreateException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ capstone/java/src/main/java/com/techelevator/dao/exceptions/DeleteException.java
@@ -0,0 +1,7 @@
+package com.techelevator.dao.exceptions;
+
+public class DeleteException extends RuntimeException{
+    public DeleteException(Throwable cause) {
+        super(cause);
+    }
+}
