4291e46e0a6af860f7a549420afd4e5c4e53a956
Kirk Williams <williams.kirk.d@gmail.com>
2022-12-08 14:41:43 -0500

    Ordered the return of all groups alphabetically

---
+++ capstone/vue/src/components/GroupDetails.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h2 class="group-name">{{}}</h2>
+      
+  </div>
+</template>
+
+<script>
+export default {
+
+    name: 'group-details',
+    props: {group: Object}
+
+}
+</script>
+
+<style>
+
+</style>
