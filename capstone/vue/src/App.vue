45b075e331b913b5e4cb9e4b7001f86fd139bf9a
Andrew Kuhlman <drewkuhlman00@gmail.com>
2022-12-08 14:39:29 -0500

    login/registration css

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -9,6 +9,19 @@
   </div>
 </template>
 <style>
+/* #app {
+  display:flex;
+  font-family: Georgia, 'Times New Roman', Times, serif;
+  font-weight: light;
+  display: flex;
+  height: 80vh;
+  flex-direction: column;
+  justify-content: first baseline;
+  align-items: center;
+  padding-top: 5%;
+  padding-bottom: 5%;
+  margin-bottom:  auto;
+} */
 h1 {
   text-align: center;
 }
