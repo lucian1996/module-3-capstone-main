f290f2637f7e87e27fc941d31ef31487691b7cf7
Andrew Kuhlman <drewkuhlman00@gmail.com>
2022-12-12 22:14:01 -0500

    claimed toggle

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -7,7 +7,7 @@ INSERT INTO users (username,password_hash,role) VALUES ('string','$2a$08$UkVvwpU
 --INSERT INTO users (username,password_hash,role) VALUES ('user4','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 --INSERT INTO users (username,password_hash,role) VALUES ('user5','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO groups (group_owner, group_name, group_code, date_created) values (1, 'test', 111, '10-10-2020');
-INSERT INTO list (group_id, list_title, description, claimed, date_modified) VALUES (1001, 'testList', 'test description', '0', 'date');
+INSERT INTO list (group_id, list_title, description, claimed, date_modified) VALUES (1001, 'testList', 'test description', 'o', 'date');
 INSERT INTO list_item(date_modified, quantity, last_modifier, list_id, group_id, item_name) VALUES ('change', 10, 1, 2001, 1001, 'cool name for item');
 INSERT INTO group_member(user_id, group_id, username, date_joined) VALUES (1, 1001, 'fridgrr member', 'date');
 COMMIT TRANSACTION;
--- capstone/vue/src/components/ListDetails.vue
+++ capstone/vue/src/components/ListDetails.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
     <div>{{list}}</div>
-      <div id="toggleClaim" @click="isClaimed = !isClaimed">
-        <div v-if="isClaimed">
-          <button>Claim</button> <br>
+      <div id="toggleClaim" >
+        <div v-if="isClaimed" @click="isClaimed = !isClaimed">
+          <button >Claim</button> <br>
         </div>
-        <div v-else>
-          <button>unClaim</button> <br>
+        <div v-show="isClaimed">
+          <button v-show="this.$store.state.user.id==list.claimedID">unClaim</button> <br>
+          <div v-show="this.$store.state.user.id!=list.claimedID">
+            List Claimed by
+          </div>
         </div>
       </div>
-
-    <div>
+      <div>
         <item-card 
         v-for="item in items"
-        v-bind:key = "item.itemId"
+        v-bind:key="item.itemId"
         v-bind:item="item"
         />
         {{items}}
@@ -22,44 +24,38 @@
 </template>
 
 <script>
-import ItemService from '../services/ItemService'
-import ItemCard from './ItemCard.vue'
+import ItemService from "../services/ItemService";
+import ItemCard from "./ItemCard.vue";
 export default {
   components: { ItemCard },
-  name: 'list-details',
+  name: "list-details",
   data() {
     return {
       items: [],
-      isClaimed: false
+      isClaimed: true,
     };
   },
-  methods: { 
-    getIsClaimed() {
-      // if ({{ this.$store.state.user.username }} 
-      //   == {{ this.$store.state.list.claimedId }}) {
-      //   isClaimed = true
-      // }
-    },
+  methods: {
+    // getIsClaimed() {
+    //   if ( this.$store.state.user.userId
+    //     == this.$store.state.list.claimedId ) {
+    //     isClaimed = true
+    //   }
+    // },
   },
-  created () {
-    ItemService.getItems(this.$route.params.list.groupId, this.$route.params.list.listId)
-    .then(response => {
+  created() {
+    ItemService.getItems(
+      this.$route.params.list.groupId,
+      this.$route.params.list.listId
+    ).then((response) => {
       this.items = response.data;
-      console.log("here are items", this.items)
-    })
+      console.log("here are items", this.items);
+    });
   },
-  computed : {
-    list () {
-      return this.$route.params.list
+  computed: {
+    list() {
+      return this.$route.params.list;
     },
-    claimStatus() {
-      // if (isClaimed) {
-      //    return {{ this.$store.state.list.claimedId }} == null;
-      // } else {
-      //    return {{ this.$store.state.list.claimedId }} == {{ this.$store.state.user.username }};
-      // } 
-      return null
-    }
   },
 };
 </script>
