9da7db30c34afbc079a7522308f4cc2715ee08f1
Elijah Grantham <elijahrgrantham@gmail.com>
2022-12-15 03:30:23 -0500

    randomimage testing

--- capstone/vue/src/components/ListCard.vue
+++ capstone/vue/src/components/ListCard.vue
@@ -1,5 +1,5 @@
 <template>
- <v-card-title  class="list-card text-center">
+ <v-card-title class="list-card text-center">
     <!-- <router-link
     v-bind:to="{ name: 'list-details', params: { listID: list.listId, groupID: list.groupId } }"> 
   </router-link> -->
@@ -12,14 +12,24 @@
 export default {
   name: "list-card",
   props: ["list"],
+  data() {
+    return {
+    images: ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png'],
+    resultImage: ''
+  }
+  },
   created() {
+
   },
   methods: {
     gotolistDetails() {
     this.$router.push({ name: 'list-details', params: { listID: this.list.listID, groupID: this.list.groupID } })
-  }}
-  
-};
+  }},
+  randomImage() {
+      const random = Math.floor(Math.random() * this.images.length)
+      this.resultImage = this.images[random];
+  }
+}
 </script>
 <style scoped>
 .list-card{
